70f59c98f8ce1f691f2d71c391cf130029151852
Abbosjon <abbosjon@Abbosjons-MacBook-Pro.local>
2025-04-03 17:00:47 +0500

    superset-roleaccess-fixed

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,4 +1,38 @@
 OVERRIDE_HTTP_HEADERS = {'X-Frame-Options': 'ALLOWALL'}
-TALISMAN_ENABLED = False
+TALISMAN_ENABLED = True
 ENABLE_CORS = True
-HTTP_HEADERS={"X-Frame-Options":"ALLOWALL"}
+HTTP_HEADERS = {"X-Frame-Options": "ALLOWALL"}
+FEATURE_FLAGS = {
+    "DASHBOARD_RBAC": True
+}
+TALISMAN_CONFIG = {
+    "content_security_policy": {
+        "base-uri": ["'self'"],
+        "default-src": ["'self'"],
+        "img-src": [
+            "'self'",
+            "blob:",
+            "data:",
+            "https://apachesuperset.gateway.scarf.sh",
+            "https://static.scarf.sh/",
+            # "https://cdn.brandfolder.io", # Uncomment when SLACK_ENABLE_AVATARS is True  # noqa: E501
+            "ows.terrestris.de",
+        ],
+        "worker-src": ["'self'", "blob:"],
+        "connect-src": [
+            "'self'",
+            "https://api.mapbox.com",
+            "https://events.mapbox.com",
+        ],
+        "object-src": "'none'",
+        "style-src": [
+            "'self'",
+            "'unsafe-inline'",
+        ],
+        "script-src": ["'self'", "'strict-dynamic'"],
+        "frame-ancestors": ["*"],  # Allow all domains
+    },
+    "content_security_policy_nonce_in": ["script-src"],
+    "force_https": False,
+    "session_cookie_secure": False,
+}
